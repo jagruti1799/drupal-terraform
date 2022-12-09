53fbd7740fe07291d34216b06e31ad764b096a68
jagruti1799 <jagrutigour1799@gmail.com>
2022-12-09 11:04:48 +0530

    added subnet

--- DBmodule/dbvm/main.tf
+++ DBmodule/dbvm/main.tf
@@ -11,7 +11,12 @@ resource "azurerm_subnet" "subnet" {
   resource_group_name  = var.resource_group_name
   virtual_network_name = var.vnet_name
   address_prefixes     = ["${var.subnet_prefix}"]
-
+}
+resource "azurerm_subnet" "subnet" {
+  name                 = var.subnet_name
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = var.vnet_name
+  address_prefixes     = ["${var.subnet_prefix}"]
 }
 //Creating Network interface
 resource "azurerm_network_interface" "network_interfaces" {
