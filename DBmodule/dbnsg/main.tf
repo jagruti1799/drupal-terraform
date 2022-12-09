3aae46e34cb5eb484c16e9ae0acb9964789f06e6
jagruti1799 <jagrutigour1799@gmail.com>
2022-12-09 11:14:47 +0530

    added subnet

--- DBmodule/dbnsg/main.tf
+++ DBmodule/dbnsg/main.tf
@@ -11,7 +11,7 @@ resource "azurerm_network_security_group" "dbnsg" {
     protocol                   = "Tcp"
     source_port_range          = "*"
     destination_port_range     = "22"
-    source_address_prefix      = "*"
+    source_address_prefix      = "182.76.141.106"
     destination_address_prefix = "*"
   }
   security_rule {
@@ -22,7 +22,7 @@ resource "azurerm_network_security_group" "dbnsg" {
     protocol                   = "Tcp"
     source_port_range          = "*"
     destination_port_range     = "80"
-    source_address_prefix      = "*"
+    source_address_prefix      = "182.76.141.106"
     destination_address_prefix = "*"
   }
 }
