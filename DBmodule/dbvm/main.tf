5eb43f28911f3ba23ced66c320c9c9982f591315
jagruti1799 <jagrutigour1799@gmail.com>
2022-12-09 12:16:56 +0530

    added subnet

--- DBmodule/dbvm/main.tf
+++ DBmodule/dbvm/main.tf
@@ -12,12 +12,7 @@ resource "azurerm_subnet" "subnet" {
   virtual_network_name = var.vnet_name
   address_prefixes     = ["${var.subnet_prefix}"]
 }
-resource "azurerm_subnet" "subnet" {
-  name                 = var.subnet_name
-  resource_group_name  = var.resource_group_name
-  virtual_network_name = var.vnet_name
-  address_prefixes     = ["${var.subnet_prefix}"]
-}
+
 //Creating Network interface
 resource "azurerm_network_interface" "network_interfaces" {
   name                = var.network_interfaces
@@ -29,6 +24,7 @@ resource "azurerm_network_interface" "network_interfaces" {
     subnet_id                     = azurerm_subnet.subnet.id
     private_ip_address_allocation = "Dynamic"
     public_ip_address_id          = azurerm_public_ip.publicip.id
+    public_network_access_enabled = true
   }
 
     depends_on = [
